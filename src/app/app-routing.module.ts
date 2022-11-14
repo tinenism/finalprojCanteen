cb818d80bd4efcbdd0a31f90c430658de1cb1e65
tine <medallonjustine01@gmail.com>
2022-11-14 09:04:57 +0800

    .

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'auth/signup', loadChildren: () => import('./auth/signup/signup.module').then(m => m.SignupModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/auth/signup/signup.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignupRoutingModule } from './signup-routing.module';
+import { SignupComponent } from './signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    SignupRoutingModule,
+    ReactiveFormsModule,
+   
+  ]
+})
+export class SignupModule { }
